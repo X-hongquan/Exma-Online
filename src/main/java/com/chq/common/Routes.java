88fe4a5d3cba96692c2bee0157ae4178134846b3
X-hongquan <1097274644@qq.com>
2023-12-17 09:50:11 +0800

    最近一次提交

---
+++ src/main/java/com/chq/common/Routes.java
@@ -0,0 +1,15 @@
+package com.chq.common;
+
+public interface Routes {
+   String[] LOGIN_NO_INTERCEPT={
+           "/user/rLogin",
+           "/user/login",
+           "/user/use",
+           "/user/tour",
+           "/user/isLogin",
+           "/user/download",
+           "/question/download"
+   };
+
+
+}
